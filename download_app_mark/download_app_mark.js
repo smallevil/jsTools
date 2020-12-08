08b216b6fc08c56b76ee88ba89c6caeb1e11f25a
smallevil@163.com <smallevil@163.com>
2020-12-08 15:18:51 +0800

    -

---
+++ download_app_mark/download_app_mark.js
@@ -0,0 +1,9 @@
+document.write('<link rel="stylesheet" type="text/css" href="https://tb666.top/js_tools/download_app_mark/mark.css" />');
+document.write('<div id="download-app-mark-tip" class="download-app-mark-tip"><p><img src="https://tb666.top/js_tools/download_app_mark/mark.png" alt=""/></p></div>');
+document.write("<script type='text/javascript' src='https://tb666.top/js_tools/browser.js'></script>");
+
+function showDownloadAppMarkTip()
+{
+    var markDiv = document.getElementById("download-app-mark-tip");
+    markDiv.style.display = "block";
+}
