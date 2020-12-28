7af57fe1557830016e93efea999ba835351b5e31
Tubie <smallevil@163.com>
2020-12-28 21:31:04 +0800

    update

--- download_app_mark/download_app_mark.js
+++ download_app_mark/download_app_mark.js
@@ -1,6 +1,6 @@
-document.write('<link rel="stylesheet" type="text/css" href="https://tb666.top/js_tools/download_app_mark/mark.css" />');
-document.write('<div id="download-app-mark-tip" class="download-app-mark-tip"><p><img src="https://tb666.top/js_tools/download_app_mark/mark.png" alt=""/></p></div>');
-document.write("<script type='text/javascript' src='https://tb666.top/js_tools/browser.js'></script>");
+document.write('<link rel="stylesheet" type="text/css" href="./mark.css" />');
+document.write('<div id="download-app-mark-tip" class="download-app-mark-tip"><p><img src="./mark.png" alt=""/></p></div>');
+document.write("<script type='text/javascript' src='../browser.js'></script>");
 
 function showDownloadAppMarkTip()
 {
