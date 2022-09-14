7e24c1a2b8cc977a557a964bbb4ea0af3b84c471
Tubie <smallevil@163.com>
2022-09-14 22:40:17 +0800

    Update download_app_mark.js

--- download_app_mark/download_app_mark.js
+++ download_app_mark/download_app_mark.js
@@ -1,6 +1,6 @@
-document.write('<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/smallevil/jsTools@0.3/download_app_mark/mark.css" />');
-document.write('<div id="download-app-mark-tip" class="download-app-mark-tip"><p><img src="//cdn.jsdelivr.net/gh/smallevil/jsTools@0.3/download_app_mark/mark.png" alt=""/></p></div>');
-document.write("<script type='text/javascript' src='//cdn.jsdelivr.net/gh/smallevil/jsTools@0.3/browser.js'></script>");
+document.write('<link rel="stylesheet" type="text/css" href="//js.tbcdn.net/gh/smallevil/jsTools@0.3/download_app_mark/mark.css" />');
+document.write('<div id="download-app-mark-tip" class="download-app-mark-tip"><p><img src="//js.tbcdn.net/gh/smallevil/jsTools@0.3/download_app_mark/mark.png" alt=""/></p></div>');
+document.write("<script type='text/javascript' src='//js.tbcdn.net/gh/smallevil/jsTools@0.3/browser.js'></script>");
 
 function showDownloadAppMarkTip()
 {
